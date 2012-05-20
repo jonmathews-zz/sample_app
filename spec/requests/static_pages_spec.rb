5a8215d21e951ef56e1846931a6748128469b03e
Jonathan Mathews <jdmathews@gmail.com>
2012-05-20 20:40:23 +0100

    Add user microposts

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -53,4 +53,20 @@ describe "Static pages" do
     page.should have_selector 'title', text: full_title('Sign up')
   end
 
+  describe "for signed-in users" do
+    let(:user) { FactoryGirl.create(:user) }
+    before do
+      FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
+      FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+      sign_in user
+      visit root_path
+    end
+
+    it "should render the user's feed" do
+      user.feed.each do |item|
+        page.should have_selector("li##{item.id}", text: item.content)
+      end
+    end
+  end
+
 end
